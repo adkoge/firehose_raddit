d1eee4c73517847c7570dce540693946acc53cd6
Alan Koger <adkoge@gmail.com>
2016-03-30 13:17:48 +0000

    Add edit and update function in controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -14,15 +14,26 @@ class LinksController < ApplicationController
 
   def create
     @link = Link.create(link_params)
-    redirect_to links_path
+
+    if @link.save
+      redirect_to links_path, :notice => "Your link has been added"
+    else
+      render "new"
+    end
   end
 
   def edit
-
+    @link = Link.find(params[:id])
   end
 
   def update
+    @link = Link.find(params[:id])
 
+    if @link.update_attributes(link_params)
+      redirect_to links_path, :notice => "Your link has been updated"
+    else
+      render "edit"
+    end
   end
 
   def destroy
