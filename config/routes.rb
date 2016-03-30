c44bb74b13d54459d740ef9d0d2ada5d65a75202
Alan Koger <adkoge@gmail.com>
2016-03-30 11:50:01 +0000

    Add index action and view to links controller Added index action and view to links controller and routed index as the root page. Also generated the link model with a title:string and url:string and migrated the database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Raddit::Application.routes.draw do
+  root 'links#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
